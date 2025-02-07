3633d1f8cb05efb93f7bdb0a593e56f664ea95a0
pmathpal1 <89689313+pmathpal1@users.noreply.github.com>
2025-02-07 11:28:29 +0530

    Add files via upload

---
+++ igw.tf
@@ -0,0 +1,11 @@
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "AWS-IGW"
+
+  }
+}
+output "IGWInformation" {
+  value = ["${aws_internet_gateway.gw.id}"]
+}
